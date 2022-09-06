b814139cd2c6b4810a1c126cac2dfa9f2d75e5cf
Itadoriii <86499855+Itadoriii@users.noreply.github.com>
2022-09-06 01:37:06 -0300

    Node y Express
    Se configuro el puerto 3000

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const productos = [
+    {
+        id:1,
+        name: "Nombre",
+        precio: 50,
+        imagen: "img/img carrusel/carrusel 3.jpg",
+        stock: 30
+    },
+    {
+        id:1,
+        name: "Nombre",
+        precio: 50,
+        imagen: "img/img carrusel/carrusel 3.jpg",
+        stock: 30
+    },
+    {
+        id:1,
+        name: "Nombre",
+        precio: 50,
+        imagen: "img/img carrusel/carrusel 3.jpg",
+        stock: 30
+    },
+    {
+        id:1,
+        name: "Nombre",
+        precio: 50,
+        imagen: "img/img carrusel/carrusel 3.jpg",
+        stock: 30
+    },
+    {
+        id:1,
+        name: "Nombre",
+        precio: 50,
+        imagen: "img/img carrusel/carrusel 3.jpg",
+        stock: 30
+    }
+]
+
+
+app.get('/api/productos', (req, res) => {
+  res.send(productos)
+})
+app.use('/',express.static("Tienda"));
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
